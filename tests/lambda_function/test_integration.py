5f48c1970016e93066ede7814b416a8a47b413a4
David Andersson <anderssonpublic@gmail.com>
2019-10-12 14:49:04 +1100

    Add integration test without namespace

--- tests/lambda_function/test_integration.py
+++ tests/lambda_function/test_integration.py
@@ -37,10 +37,48 @@ def test_deployment_create(nginx_deployment):
     """
     deployment_dict, namespace, name = nginx_deployment
 
-    operations.create(body=deployment_dict)
+    result = operations.create(body=deployment_dict)
 
+    # Check result
+    assert result.status == "SUCCESS"
+    assert result.physical_name == f"{namespace}/{name}"
     # Check that the deployment is created
     apps_v1_api = kubernetes.client.AppsV1Api()
     response = apps_v1_api.list_namespaced_deployment(namespace=namespace)
     assert response.items
     assert response.items[0].metadata.name == name
+
+
+@pytest.fixture
+def namespace():
+    """Kubernetes namespace."""
+    path = "tests/lambda_function/kubernetes_fixtures/namespace.yaml"
+    with open(path) as in_file:
+        deployment_dict = yaml.safe_load(in_file)
+    name = "namespace-1"
+
+    yield deployment_dict, name
+
+    core_v1_api = kubernetes.client.CoreV1Api()
+    core_v1_api.delete_namespace(name=name)
+
+
+@pytest.mark.integration
+def test_namespace_create(namespace):
+    """
+    GIVEN namespace as dictionary
+    WHEN create is called with the namespace dictionary as a body
+    THEN the namespace is created.
+    """
+    namespace_dict, name = namespace
+
+    result = operations.create(body=namespace_dict)
+
+    # Check result
+    assert result.status == "SUCCESS"
+    assert result.physical_name == name
+    # Check that the namespace is created
+    core_v1_api = kubernetes.client.CoreV1Api()
+    response = core_v1_api.list_namespace()
+    assert response.items
+    assert name in {item.metadata.name for item in response.items}
