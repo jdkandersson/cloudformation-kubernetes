86853a712d2112f885811c57bea8b92deea01f2d
David Andersson <anderssonpublic@gmail.com>
2019-10-20 17:53:47 +1100

    Move operational logic to separate functions

--- lambda_function/index.py
+++ lambda_function/index.py
@@ -93,47 +93,11 @@ def lambda_handler(event, _context):
     }
 
     if parameters.request_type == "Create":
-        result = operations.create(body=parameters.resource_properties)
-        response_body["Status"] = result.status
-        if result.status == "SUCCESS":
-            response_body["PhysicalResourceId"] = result.physical_name
-        else:
-            response_body[
-                "PhysicalResourceId"
-            ] = f"{FAIL_PHYSICAL_NAME_PREFIX}{parameters.logical_resource_id}"
-            response_body["Reason"] = result.reason
+        _handle_create(parameters=parameters, response_body=response_body)
     elif parameters.request_type == "Update":
-        # Check that physical resource id is given
-        if parameters.physical_resource_id is None:
-            raise exceptions.MalformedEventError(
-                "PhysicalResourceId is required for Update event."
-            )
-        result = operations.update(
-            body=parameters.resource_properties,
-            physical_name=parameters.physical_resource_id,
-        )
-        response_body["Status"] = result.status
-        response_body["PhysicalResourceId"] = parameters.physical_resource_id
-        if result.status == "FAILURE":
-            response_body["Reason"] = result.reason
+        _handle_update(parameters=parameters, response_body=response_body)
     elif parameters.request_type == "Delete":
-        # Check that physical resource id is given
-        if parameters.physical_resource_id is None:
-            raise exceptions.MalformedEventError(
-                "PhysicalResourceId is required for Update event."
-            )
-        response_body["PhysicalResourceId"] = parameters.physical_resource_id
-        # Checking for delete due to failed create
-        if parameters.physical_resource_id.startswith(FAIL_PHYSICAL_NAME_PREFIX):
-            response_body["Status"] = "SUCCESS"
-        else:
-            result = operations.delete(
-                body=parameters.resource_properties,
-                physical_name=parameters.physical_resource_id,
-            )
-            response_body["Status"] = result.status
-            if result.status == "FAILURE":
-                response_body["Reason"] = result.reason
+        _handle_delete(parameters=parameters, response_body=response_body)
     else:
         raise exceptions.MalformedEventError(
             f"{parameters.request_type} RequestType has not been implemented."
@@ -144,3 +108,82 @@ def lambda_handler(event, _context):
     pool.request(
         "PUT", parameters.response_url, body=json.dumps(response_body).encode("utf-8")
     )
+
+
+def _handle_create(
+    *, parameters: Parameters, response_body: typing.Dict[str, str]
+) -> None:
+    """
+    Handle create event.
+
+    Args:
+        parameters: Event parameters.
+        response_body: The body being assembled for the response.
+
+    """
+    result = operations.create(body=parameters.resource_properties)
+    response_body["Status"] = result.status
+    if result.physical_name is not None:
+        response_body["PhysicalResourceId"] = result.physical_name
+    if result.status == "FAILURE":
+        response_body[
+            "PhysicalResourceId"
+        ] = f"{FAIL_PHYSICAL_NAME_PREFIX}{parameters.logical_resource_id}"
+    if result.reason is not None:
+        response_body["Reason"] = result.reason
+
+
+def _handle_update(
+    *, parameters: Parameters, response_body: typing.Dict[str, str]
+) -> None:
+    """
+    Handle update event.
+
+    Args:
+        parameters: Event parameters.
+        response_body: The body being assembled for the response.
+
+    """
+    # Check that physical resource id is given
+    if parameters.physical_resource_id is None:
+        raise exceptions.MalformedEventError(
+            "PhysicalResourceId is required for Update event."
+        )
+    result = operations.update(
+        body=parameters.resource_properties,
+        physical_name=parameters.physical_resource_id,
+    )
+    response_body["Status"] = result.status
+    response_body["PhysicalResourceId"] = parameters.physical_resource_id
+    if result.reason is not None:
+        response_body["Reason"] = result.reason
+
+
+def _handle_delete(
+    *, parameters: Parameters, response_body: typing.Dict[str, str]
+) -> None:
+    """
+    Handle delete event.
+
+    Args:
+        parameters: Event parameters.
+        response_body: The body being assembled for the response.
+
+    """
+    # Check that physical resource id is given
+    if parameters.physical_resource_id is None:
+        raise exceptions.MalformedEventError(
+            "PhysicalResourceId is required for Update event."
+        )
+    response_body["PhysicalResourceId"] = parameters.physical_resource_id
+    # Checking for delete due to failed create
+    if parameters.physical_resource_id.startswith(FAIL_PHYSICAL_NAME_PREFIX):
+        response_body["Status"] = "SUCCESS"
+    else:
+        result = operations.delete(
+            body=parameters.resource_properties,
+            physical_name=parameters.physical_resource_id,
+        )
+        response_body["Status"] = result.status
+        if result.reason is not None:
+            response_body["Reason"] = result.reason
